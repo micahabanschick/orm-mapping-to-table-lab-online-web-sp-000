a30e0e1bf47b74bb3ede74c243fce6d45a1031ea
Micah Banschick <micha.ban@gmail.com>
2020-03-23 16:49:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,48 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  attr_accessor :name, :grade
+  attr_reader :id 
+  
+  @@all = []
+  
+  def initialize(name, grade)
+    @name = name 
+    @grade = grade 
+    
+    @@all << self 
+    @id = Student.all.length - 4
+  end
+  #binding.pry 
+  def self.all 
+    @@all 
+  end 
+  
+  def self.create_table
+    create_student_table =  
+      "CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT);"
+    DB[:conn].execute(create_student_table)
+  end 
+  
+  def self.drop_table
+    drop_student_table =  
+      "DROP TABLE IF EXISTS students;"
+    DB[:conn].execute(drop_student_table)
+  end 
+  
+  def save 
+    # Student.create_table 
+    new_student_row = 
+      "INSERT INTO students (id, name, grade) VALUES (?, ?, ?)"
+    DB[:conn].execute(new_student_row, self.id, self.name, self.grade)
+  end 
+  
+  def self.create(name: name, grade: grade) 
+    new_student_instance = self.new(name, grade)
+    new_student_instance.save
+  end 
+  
 end
