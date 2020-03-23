cd1e6c6068b0e1cf1dfdb77940979b099cac9ff1
Micah Banschick <micha.ban@gmail.com>
2020-03-23 17:34:16 +0000

    Automatically backed up by Learn

---
+++ lib/student_trials.sql
@@ -0,0 +1,11 @@
+.headers on 
+.mode column
+.width auto 
+
+DROP TABLE IF EXISTS students 
+
+CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT);
+      
