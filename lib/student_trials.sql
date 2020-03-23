4f17e95c2585e9282d7787ebcc0250b1bd2a1fc5
Micah Banschick <micha.ban@gmail.com>
2020-03-23 17:35:43 +0000

    Automatically backed up by Learn

--- lib/student_trials.sql
+++ lib/student_trials.sql
@@ -1,10 +1,10 @@
--- sqlite3 db/students.db <
+-- sqlite3 db/students.db < student_trials.sql
 
 .headers on 
 .mode column
 .width auto 
 
-DROP TABLE IF EXISTS students 
+DROP TABLE IF EXISTS students;
 
 CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
