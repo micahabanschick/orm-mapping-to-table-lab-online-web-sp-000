8c68960f05ece8757c07d1b36740ed8e57678dc5
Micah Banschick <micha.ban@gmail.com>
2020-03-23 17:37:13 +0000

    Automatically backed up by Learn

--- lib/student_trials.sql
+++ lib/student_trials.sql
@@ -11,4 +11,6 @@ CREATE TABLE IF NOT EXISTS students (
       name TEXT,
       grade TEXT);
 
+INSERT INTO students (id, name, grade) VALUES (1, "M", "9th")
+
 SELECT id FROM students DESC LIMIT 1      
